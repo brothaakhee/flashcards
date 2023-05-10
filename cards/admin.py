464a34bad7e7b7cedc9b25a53cad17af91995316
Hammad Hai <hammad.a.hai@gmail.com>
2023-05-10 16:49:11 -0400

    Add Word to admin

--- cards/admin.py
+++ cards/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Word
 
-# Register your models here.
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "word",
+        "definition",
+        "bin_number",
+        "next_review",
+        "wrong_attempts",
+    )
+
+
+admin.site.register(Word, WordAdmin)
