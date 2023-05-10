772c072854fb60b5448033abc224dacd75df381a
Hammad Hai <hammad.a.hai@gmail.com>
2023-05-10 00:38:21 -0400

    Add missing import

--- cards/views.py
+++ cards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views, authenticate, login
@@ -6,28 +6,28 @@ from django.contrib.auth import views as auth_views, authenticate, login
 
 @login_required
 def flashcards(request):
-    return render(request, 'flashcards.html')
+    return render(request, "flashcards.html")
 
 
 @login_required
 def user_words(request):
-    return render(request, 'user_words.html')
+    return render(request, "user_words.html")
 
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
             user = authenticate(request, username=username, password=password)
             login(request, user)
-            return redirect('flashcards')
+            return redirect("flashcards")
     else:
         form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, "register.html", {"form": form})
 
 
 class LoginView(auth_views.LoginView):
-    template_name = 'login.html'
+    template_name = "login.html"
